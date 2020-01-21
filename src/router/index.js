429d9b01f330f3e133cbac4d798aaa2d371ee82d
sunnysu <Zxx18670213951@163.com>
2020-01-22 01:15:54 +0800

    黑客特效

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import cracker from '../components/cracker.vue'
+
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'cracker',
+    component: cracker
   },
   {
     path: '/about',
